14c450f6b184ac725fce9e26c5327dc49858c80a
CL3372 <chocolatesdomjose@gmail.com>
2026-02-09 15:46:43 +0000

    inserted function formatday timestamp, icans now change so does weather

--- src/index.js
+++ src/index.js
@@ -59,6 +59,13 @@ function searchCity(city) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "aa9340df384978aa7t1a53a8fabcc2o1";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -68,28 +75,29 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-<div class="weather-forecast-day">
-           <div class="weather-forecast-date">${day}</div>
-           <div class="weather-forecast-icon">⛅</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 7) {
+      forecastHtml =
+        forecastHtml +
+        `
+           <div class="weather-forecast-day">
+           <div class="weather-forecast-date">${formatDay(day.time)}</div>
+           
+           <img src="${day.condition.icon_url} "class="weather-forecast-icon" />
            <div class="weather-forecast-temperatures">
            <div class="weather-forecat-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
            </div>
-           <div class="weather-forecast-temperature">9º</div>
+           <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
           </div>
           </div>  
         `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
-
 searchCity("Lisbon");
